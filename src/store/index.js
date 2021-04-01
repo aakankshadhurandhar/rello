0afa2352bff12f823a875e89b2edda76ee287c96
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-04-01 17:22:27 +0530

    Created store object in index.js
    retuens the whole state tree of the app

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+//file for creating store(container) and pass it a empty reducer
+
+
+import { createStore } from 'redux';
+//creates a complete state tree of app
+
+
+//empty reducer for now
+const reducer()=>{}
+
+
+const store=createStore(reducer);
+//it returns complte state of app
+
+export default store;
+
