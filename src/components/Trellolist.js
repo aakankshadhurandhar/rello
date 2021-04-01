41c48f530d287ef488e1275f51e62e232b2648a5
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-04-01 19:06:16 +0530

    added css to trellolist

--- src/components/Trellolist.js
+++ src/components/Trellolist.js
@@ -1,11 +1,20 @@
 //this file is for whole list component like :add a task etc
-
+import './Components.css'
 
 const Trellolist = ({title}) => {
     return (
-        <div>
+        <div className="listclass" style={styles.container}>
             <p>{title}</p>
         </div>
     )
 }
+const styles = {
+    container: {
+     
+      
+      width: 300,
+      padding: 8,
+    },
+  };
+
 export default Trellolist;
